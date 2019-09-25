6f1c5ed997dcd38e6470bc74da2be1a8fb2751dd
unknown <james.da.4th@gmail.com>
2019-09-25 13:49:02 -0700

    remove old code

--- steamify/models.py
+++ steamify/models.py
@@ -4,15 +4,6 @@ from django.db import models
 from django.urls import reverse
 
 
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField('date published')
-#
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
-
 
 def rangeTuple(start, endInclusive):
     return [(x, str(x)) for x in range(start, endInclusive + 1)]
