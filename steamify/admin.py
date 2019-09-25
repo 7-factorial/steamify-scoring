263bf37d945fcecd85cb9d26c1b289c5496a8981
O'Meara <jomeara@txt.textron.com>
2019-09-25 14:41:39 -0400

    reactivate admin (not sure if we'll keep this)

--- steamify/admin.py
+++ steamify/admin.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-# from .models import Question, Choice
-# admin.site.register(Question)
-# admin.site.register(Choice)
 
 # Not sure if we'll use this approach.
-# from .models import EngMiddle, Shared, VisualArtsMiddle
+from .models import EngMiddle   # , Shared, VisualArtsMiddle
 
-# admin.site.register(EngMiddle)
+admin.site.register(EngMiddle)
 # admin.site.register(VisualArtsMiddle)
