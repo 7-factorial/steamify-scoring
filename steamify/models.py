e5671e6905c7a39d8fc14aa7324104450f002c1b
O'Meara <jomeara@txt.textron.com>
2019-09-29 21:28:02 -0400

    add engmid rubric; add funcs for displaying html help text and DRY fields

--- steamify/models.py
+++ steamify/models.py
@@ -1,13 +1,41 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.html import format_html
+
 
 # Create your models here.
 from django.urls import reverse
 
 
 
-def rangeTuple(start, endInclusive):
-    return [(x, str(x)) for x in range(start, endInclusive + 1)]
+# def rangeTuple(start, endInclusive):
+#     return [(x, str(x)) for x in range(start, endInclusive + 1)]
+
+
+def labeledRangeTuple():
+    return [
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5")
+    ]
+
+
+def standardSteamifyField(help_text_unproc, verbose_name=None):
+    return models.IntegerField(
+        choices=labeledRangeTuple(),
+        help_text=htmlListify(help_text_unproc),
+        verbose_name=verbose_name)
+    
+
+def htmlListify(rawstr):
+    # type: (str) -> str
+    spl = rawstr.splitlines()
+    stripped = [x.strip() for x in spl]
+    noEmpties = [x for x in stripped if x]
+    with_lis = "\n".join(format_html("<li>{}</li>", x) for x in noEmpties)
+    return "<ul>{}</ul>".format(with_lis)
 
 
 class Team(models.Model):
@@ -21,9 +49,9 @@ class Team(models.Model):
 
 
 class Shared(models.Model):
-    something_shared = models.IntegerField(
-        choices=rangeTuple(1, 5),
-        help_text="Did they do somethign well that involes all competitions?")
+    # something_shared = models.IntegerField(
+    #     choices=labeledRangeTuple(),
+    #     help_text="Did they do somethign well that involes all competitions?")
     judge = models.ForeignKey(
         get_user_model(),
         on_delete=models.PROTECT,
@@ -56,10 +84,28 @@ class Shared(models.Model):
 
 # each instance of this will be a single score submission by one judge
 class EngMiddle(Shared):
-    presentation_quality = models.IntegerField(
-        choices=rangeTuple(1, 5),
-        help_text="Did they present well? Did they do the things?")
     TLA = "M.EN"
+    presentation = standardSteamifyField("""
+        Presentation demonstrates understanding of the material.
+        Presenters facilitate engaging discussion regarding challenge expectations.  
+        Presenters explain their research concerning the CO2 device as it relates to environmental sustainability.
+        The purpose of the design is clear, realistic, and presenters share how it may be scaled up.
+        Most students have an explicit part in either the explanation or demonstration of the design process regarding the prototype; however, one or two may function as support.
+        """)
+    design_notebook = standardSteamifyField("""
+        Relevant research from more than 3 different reputable resources is included and documented in the design notebook in “Resources” section.
+        Design process somewhat chronicled.
+        Design notebook quality is at standard (Table of contents is inaccurate, page numbers are mostly correct, titles and dates are somewhat clear, entries and figures are clear).
+        """)
+    engineering_design_prototype_working_model = standardSteamifyField("""
+        The operation and design of the prototype somewhat fulfill the challenge, but is unclear in some aspects.
+        """, verbose_name="Engineering Design/Prototype/Working Model")
+    engineering_statement = standardSteamifyField("""
+        Written statement submitted at the time of performance.
+        Statement is neat and has 1-3 spelling/grammatical/punctuation errors.
+        Statement links standards to key elements and outcome, but may be unclear in one or two places.""")
+    
+    
     # pub_date = models.DateTimeField('date published')
 
 
@@ -71,7 +117,7 @@ class VisualArtsMiddle(Shared):
 
 class VisualArtsElem(Shared):
     artistic_artsyness = models.IntegerField(
-        choices=rangeTuple(1, 5),
+        choices=labeledRangeTuple(),
         help_text="Are they artsy? OR Fartsy?")
     TLA = "E.VA"
 
