dc26273e95e84f91524bcf278a86e7174d230b67
James O'Meara <jomeara@textron.com>
2019-10-08 10:32:15 -0400

    team importing (modified commit without team data)

---
+++ steamify/management/commands/deleteallteams.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from steamify.models import Team
+
+
+## commented for safety
+# class Command(BaseCommand):
+
+#     def handle(self, *args, **options):
+#         Team.objects.all().delete()
+        
+        
---
+++ steamify/management/commands/importteams.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from steamify.models import Team
+import csv
+import os
+
+
+def readOneFile(fpath):
+    with open(fpath) as f:
+        reader = csv.DictReader(f)
+        for line in reader:
+            Team.objects.create(
+                dotted_id=line["dotted_id"].strip(),
+                name=line["name"].strip(),
+                school_name=line["school_name"].strip(),
+                )
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        readOneFile(os.path.join("miscdata", "teamData.csv"))
+        
