d7582da46025c719f66dd33f862d652f8f7866f3
James O'Meara <jomeara@textron.com>
2019-10-09 10:56:24 -0400

    nothing (remove old todo comment)

--- steamify/forms.py
+++ steamify/forms.py
@@ -8,8 +8,6 @@ class PickTeamIdForm(forms.Form):
     def clean_full_team_id(self):
         ftid = self.cleaned_data['full_team_id']
 
-        # Maybe TODO: should the teamnames be in a text file?
-        # if ftid not in ["hardcoded", "list", "of", "teams", "which", "definitely", "should", "not", "be", "hardcoded", "E.VA.156", "E.EN.278", "M.RO.390", "M.EN.432"]:
         if not Team.objects.filter(dotted_id=ftid).exists():  # pylint: disable=no-member
             raise forms.ValidationError("Error: did not recognize the team id '{}'. Try again.".format(ftid))
 
