43a7cfec73cf12ea42b1319bb457b11a408c4fc1
James O'Meara <jomeara@textron.com>
2019-10-16 15:12:08 -0400

    pretty radio buttons without a '----' label!

--- steamify/urls.py
+++ steamify/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
+from django import db
+
 from django.views.generic import TemplateView
 
 from . import views
 from .views import GenericCreate, PickTeamIdView, GenericDetail, GenericUpdate, EntryHomeView
 from .models import ALL_EXCEPT_SPONT, Spont, Shared
 from .utils.misc import shared_fields_to_exlude_in_user_forms
-from django.forms import modelform_factory
-from typing import List, Callable, Type
+from django.forms import modelform_factory, RadioSelect
+from django import forms
+from django.forms.widgets import Input
+from typing import List, Callable, Type, Dict, Set, Tuple, Any
 
 
 app_name = 'steamify'
@@ -23,11 +27,34 @@ urlpatterns = [
 ]  # type: List[Callable]
 
 
+def _getUserDisplayedAttrs(ModelClass):
+    # type: (Type[Shared]) -> Set[str]
+    names = {x.name for x in ModelClass._meta.get_fields()}  # type: Set[str]
+    return names - {"id", "shared_ptr"} - set(shared_fields_to_exlude_in_user_forms)
+    # the next two commented lines are from an older attempt.
+    # widgets = dict((nam, RadioSelect) for nam in namesForDict)  # type: Dict[str, Input]
+    # return widgets
+
+
 def run_as_view(ViewClass, ModelClass):
-    return ViewClass.as_view(
-            model=ModelClass,
-            form_class=modelform_factory(ModelClass,
-                exclude=shared_fields_to_exlude_in_user_forms))
+    # type: (Type[Any], Type[Shared]) -> Any
+    keepers = _getUserDisplayedAttrs(ModelClass)
+    
+    # This took me a little while to figure out, so I posted it on StackOverflow:
+    # https://stackoverflow.com/questions/58383617/django-how-to-specify-include-blank-false-in-modelform-factory-using-formf/
+    def formfield_callback(f, **kwargs):
+        # type: (db.models.fields.Field, dict) -> forms.Field
+        if f.name in keepers:
+            kwargs['choices'] = f.get_choices(include_blank=False)
+            kwargs['widget'] = RadioSelect
+        # regardless of whether I modified it, call .formfield()
+        return f.formfield(**kwargs)
+
+    modForm = modelform_factory(ModelClass,
+                exclude=shared_fields_to_exlude_in_user_forms,
+                formfield_callback=formfield_callback,
+                )
+    return ViewClass.as_view(model=ModelClass, form_class=modForm)
 
 
 def createUrls(ModelClass, tla_ish):
