9c6a782198c7b21a22dd8bfde6ab8d947a26d0d5
James O'Meara <jomeara@textron.com>
2019-10-22 16:30:11 -0400

    add deletion management commands for all models

---
+++ steamify/management/commands/deleteall.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from steamify.models import Team, ALL_COMPETS, Shared
+from django.contrib.auth.models import User
+
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):    
+        ## commented for safety
+
+        # Team.objects.all().delete()
+        
+        # for Compet in ALL_COMPETS:
+            # Compet.objects.all().delete()
+
+        # User.objects.filter(is_staff=False).delete()
+
+        # AllowedDevice.objects.all().delete()
+
+        # shareds = list(Shared.objects.all())
+        # if shareds:
+        #     print(shareds)
+        #     Shared.objects.all().delete()
+        
+        pass
