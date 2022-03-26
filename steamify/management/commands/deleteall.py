3416d8ac1cecd1998fc28f866af510b4407d71ca
j <james.da.4th@gmail.com>
2022-03-25 20:07:10 -0400

    minor: for deleteall, add comments  and remove AllowedDevices

--- steamify/management/commands/deleteall.py
+++ steamify/management/commands/deleteall.py
@@ -9,17 +9,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):    
         ## commented for safety
 
+        # print("Deleting the following:")
+        # print(" - all score submissions")
         # for Compet in ALL_COMPETS:
             # Compet.objects.all().delete()
 
+        # print(" - all Teams")
         # Team.objects.all().delete()
 
+        # print(" - all Users (except 'staff'")
         # User.objects.filter(is_staff=False).delete()
-
-        # AllowedDevice.objects.all().delete()
 
         # shareds = list(Shared.objects.all())
         # if shareds:
+        #     raise ValueError("There shouldn't be 'shareds' at this point.")
         #     print(shareds)
         #     Shared.objects.all().delete()
         
